2f4cecbbc36f5d2705227ab71585b34894d97f4c
Elyon9612 <lihongh@oregonstate.edu>
2022-02-01 01:28:38 -0800

    3 second approach

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // First approcach:
-        if(s.length() ==0)  return 0;
+        // Second approcach:
+        vector<int> table(256,0);
+        // unordered_map<char,int> table;
+        // Right node go first until find table[right] > 1 then go left node untill table[right] == 1
+        // result = max(r-l+1)
         int result = 0;
-
-        unordered_map<int,int> table;
-        int left = 0,right = 0;
-        while(right < s.length()){
-            table[s[right]] ++;
-            while(table[s[right]] > 1){
-                table[s[left]] --;
-                left ++;
+        int l = 0,r = 0;
+        for(r=0;r<s.length();r++){
+            table[s[r]] ++;
+            while(table[s[r]] > 1){
+                table[s[l]] --;
+                l ++;
             }
-            result = max(result,right - left + 1);
-            right ++;
+            result = max(result,r-l+1);
         }
+        
         return result;
     }
 };
