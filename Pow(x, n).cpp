57c467c72554a73251bd3258e44e3a98bdd6074a
Elyon9612 <lihongh@oregonstate.edu>
2022-02-04 21:15:26 -0800

    recursive for Pow

--- Pow(x, n).cpp
+++ Pow(x, n).cpp
@@ -14,3 +14,28 @@ public:
         return result;
     }
 };
+
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        // Second approach with 2^n = 2^(2*(n/2))
+        long long N = n;
+        if(N < 0){
+            N = -N;
+            x = 1/x;
+        }
+        
+        return helper(x,N);
+    }
+private:
+    double helper(double x,long long n){
+        if(n == 0)  return 1.0;
+        
+        double half = helper(x,n/2);
+        if(n % 2 ==0)
+            return half * half;
+        else
+            return half * half * x;
+    }
+};
